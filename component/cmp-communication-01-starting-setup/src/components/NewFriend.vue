691b8441e54eb3ff6c661ba284306a5f60861965
Tidiane <tijdrame@gmail.com>
2024-04-26 09:19:06 -0400

    design

--- component/cmp-communication-01-starting-setup/src/components/NewFriend.vue
+++ component/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -1,21 +1,25 @@
 <template>
-  <form @submit.prevent="submitData">
-    <div>
-      <label>Name</label>
-      <input type="text" v-model="enteredName" />
-    </div>
-    <div>
-      <label>Phone</label>
-      <input type="tel" v-model="enteredPhone" />
-    </div>
-    <div>
-      <label>Email</label>
-      <input type="email" v-model="enteredEmail" />
-    </div>
-    <div>
-      <button>Add Contact</button>
-    </div>
-  </form>
+  <ul>
+    <li>
+      <form @submit.prevent="submitData">
+        <div>
+          <label>Name</label>
+          <input type="text" v-model="enteredName" />
+        </div>
+        <div>
+          <label>Phone</label>
+          <input type="tel" v-model="enteredPhone" />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" v-model="enteredEmail" />
+        </div>
+        <div>
+          <button>Add Contact</button>
+        </div>
+      </form>
+    </li>
+  </ul>
 </template>
 <script>
 export default {
