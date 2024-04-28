251b2618c864d4b810616a818efdaf023d56bc55
Tidiane <tijdrame@gmail.com>
2024-04-28 16:19:39 -0400

    dynamic component

--- component/deep/cmp-adv-01-starting-setup/src/App.vue
+++ component/deep/cmp-adv-01-starting-setup/src/App.vue
@@ -1,40 +1,45 @@
 <template>
   <div>
     <the-header></the-header>
-    <BadgeList />
-    <User-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></User-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps["anotherProp"] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goal')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goal')">Manage Goals</button>
+    <!--active-goal v-if="selectedComponent === 'active-goal'"></active-goal>
+    <manage-goal v-if="selectedComponent === 'manage-goal'"></manage-goal-->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+//import BadgeList from "./components/BadgeList.vue";
+//import UserInfo from "./components/UserInfo.vue";
+//import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoal from "./components/ActiveGoal.vue";
+import ManageGoal from "./components/ManageGoal.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    //BadgeList,
+    //UserInfo,
+    //CourseGoals,
+    ActiveGoal,
+    ManageGoal,
   },
   data() {
     return {
+      selectedComponent: "active-goal",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
