2325733b5c63d7a31a59ce61b0b4e0d6091bd536
Tidiane <tijdrame@gmail.com>
2024-04-28 20:54:50 -0400

    init

---
+++ component/deep/prj-cmp-01-starting-setup/src/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="it in storedResources" :key="it.id">{{ it }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
