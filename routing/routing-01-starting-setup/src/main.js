bc70f4779f618f0572d1816ea03ffb061650d3ca
Tidiane <tijdrame@gmail.com>
2024-04-30 21:57:01 -0400

    clic & pass data

--- routing/routing-01-starting-setup/src/main.js
+++ routing/routing-01-starting-setup/src/main.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
@@ -10,6 +11,7 @@ const router = createRouter({
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   linkActiveClass: 'active',
 });
