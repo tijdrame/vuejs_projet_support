5099d47b06c005fe37e0fc629a76964c607765e1
Tidiane <tijdrame@gmail.com>
2024-05-01 21:30:31 -0400

    load mult components

--- routing/routing-01-starting-setup/src/main.js
+++ routing/routing-01-starting-setup/src/main.js
@@ -4,8 +4,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
 import UsersList from './components/users/UsersList.vue';
 import NotFound from './components/nav/NotFound.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,7 +16,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
           name: 'team-members',
@@ -24,7 +26,7 @@ const router = createRouter({
         },
       ] /*, alias: '/' */,
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
 
     { path: '/:notFound(.*)', component: NotFound },
   ],
