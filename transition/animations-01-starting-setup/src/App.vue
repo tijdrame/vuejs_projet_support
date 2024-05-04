6c3fae42591eaa63dcf1008793bb287cb6c2e6a5
Tidiane <tijdrame@gmail.com>
2024-05-04 14:43:40 -0400

    animate with css

--- transition/animations-01-starting-setup/src/App.vue
+++ transition/animations-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +10,12 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { dialogIsVisible: false, animatedBlock: false };
   },
   methods: {
     showDialog() {
@@ -23,6 +23,9 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
     },
   },
 };
@@ -57,6 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /*transition: transform 0.3s ease-out;*/
 }
 .container {
   max-width: 40rem;
@@ -68,5 +72,20 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  /*transform: translateX(-150px);*/
+  animation: slide-fade 0.3s ease-out forwards;
+}
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150) scale(1);
+  }
 }
 </style>
