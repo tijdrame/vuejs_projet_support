f0d9621fc7284a108522c5998f0528dbd4eb06ad
Tidiane <tijdrame@gmail.com>
2024-05-05 22:23:30 -0400

    init vuex and base usage

---
+++ vuex/vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <base-container title="Vuex">
+    <!--h3>{{ $store.state.counter }}</h3-->
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
