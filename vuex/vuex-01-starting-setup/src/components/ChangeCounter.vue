ad199bc31ee912a2f3a8c07d5aecbaa301743ec6
Tidiane <tijdrame@gmail.com>
2024-05-07 10:50:13 -0400

    mapGetters & mapActions

--- vuex/vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex/vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,12 +1,19 @@
 <template>
-  <button @click="addOne">Add 2</button>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({ value: 11 })">Add 11</button>
 </template>
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
-    addOne() {
+    /*addOne() {
       this.$store.dispatch('increment', { value: 2 });
-    },
+    },*/
+    //...mapActions(['increment', 'increase']),
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
   },
 };
 </script>
