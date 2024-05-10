e68db74cff8be32823ef3c4d13f50c0775748382
Tidiane <tijdrame@gmail.com>
2024-05-10 16:02:28 -0400

    handle post and fetch requests

--- vuex/main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ vuex/main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -1,11 +1,49 @@
 export default {
-  contactCoach(context, payload) {
-    const newrRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
+  async contactCoach(context, payload) {
+    const newRequest = {
       userEmail: payload.email,
       message: payload.message,
     };
-    context.commit('addRequest', newrRequest);
+    const response = await fetch(
+      `https://blog-dafe6.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send request!'
+      );
+      throw error;
+    }
+    newRequest.coachId = payload.coachId;
+    newRequest.id = responseData.name; //id generer par firebase
+    context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `https://blog-dafe6.firebaseio.com/requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch requests!'
+      );
+      throw error;
+    }
+    const requests = [];
+    for (const key in responseData) {
+      const req = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(req);
+    }
+    context.commit('setRequests', requests);
   },
 };
