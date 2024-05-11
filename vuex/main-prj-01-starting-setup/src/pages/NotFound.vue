531e0a822598c8ac181ae7eed3073ac9d90c8b24
Tidiane <tijdrame@gmail.com>
2024-05-11 14:01:47 -0400

    not foud template

--- vuex/main-prj-01-starting-setup/src/pages/NotFound.vue
+++ vuex/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -1,1 +1,9 @@
-<template><p>yeah</p></template>
+<template>
+  <base-card>
+    <h2>Page Not Found!</h2>
+    <p>
+      This is page could not be found - maybe check all our
+      <router-link to="/coaches">Coaches</router-link>
+    </p>
+  </base-card>
+</template>
