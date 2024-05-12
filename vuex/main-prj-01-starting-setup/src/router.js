71ba1678b93259a20f083b1acbe91f192ad71b7f
Tidiane <tijdrame@gmail.com>
2024-05-11 20:08:59 -0400

    signup

--- vuex/main-prj-01-starting-setup/src/router.js
+++ vuex/main-prj-01-starting-setup/src/router.js
@@ -5,6 +5,7 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -19,6 +20,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
