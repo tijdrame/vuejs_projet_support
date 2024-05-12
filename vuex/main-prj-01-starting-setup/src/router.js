8fae577e0aef001e15ae9cac2fa3d1da291bb326
Tidiane <tijdrame@gmail.com>
2024-05-12 12:49:05 -0400

    load async for otimize

--- vuex/main-prj-01-starting-setup/src/router.js
+++ vuex/main-prj-01-starting-setup/src/router.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+
+//import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
+//import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+//import RequestsReceived from './pages/requests/RequestsReceived.vue';
+//import ContactCoach from './pages/requests/ContactCoach.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+//import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
 
+/*const CoachDetail = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachDetail.vue')
+);*/
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 const router = createRouter({
   history: createWebHistory(),
   routes: [
